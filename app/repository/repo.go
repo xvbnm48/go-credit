c9256d8fc45d4c2391a4440824ba4b996836aeb5
fariz.wisnu <fariz.wisnu@bluebirdgroup.com>
2023-07-16 20:00:00 +0700

    repository

--- app/repository/repo.go
+++ app/repository/repo.go
@@ -1,9 +1,21 @@
 package app
 
-import "context"
+import model "github.com/xvbnm48/go-credit/app/model"
 
 type (
 	UserRepo interface {
-		FindByID(ctx context.Context, id uuid.UUID) (model.User, error)
+		FindByID(userID string) (*model.UserAccount, error)
+		FindMerchantByID(merchantID string) (*model.UserAccount, error)
+		Save(account *model.UserAccount) error
+	}
+
+	WalletRepo interface {
+		FindByID(walletID string) (*model.Wallet, error)
+		FindByUserID(userID string) (*model.Wallet, error)
+		Save(wallet *model.Wallet) error
+	}
+	TransactionRepo interface {
+		FindByID(transactionID string) (*model.Transaction, error)
+		Save(txn *model.Transaction) error
 	}
 )
